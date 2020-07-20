d4866b5adbd193bd52e5d231e779783487940827
NikaEG <ally.via@gmail.com>
2020-07-20 15:49:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+if num == 0:
+  print("That's a positive number, that is!")
+elif num > 0:
+  print("That's a positive number, that is!")
+else:
+  print("That there is a negative number.")
